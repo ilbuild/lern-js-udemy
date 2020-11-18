4bd1ad6f934b126dea20b101a9c7a25b152521cc
Pavel <64465001+ilbuild@users.noreply.github.com>
2020-11-18 22:04:15 +0300

    Update 018script.js
    3 practice done

--- js/018script.js
+++ js/018script.js
@@ -76,5 +76,17 @@ function showMyDB(hidden) {
 
 showMyDB(personalMovieDB.privat);
 
+/* function writeYourGenres() {
+  for (let i = 1; i <= 3; i++) {
+    const genre = prompt(`Ваш любимый жанр под номером ${i}`);
+    personalMovieDB.genres[i - 1] = genre;
+  }
+} */
 
-console.log(personalMovieDB);
+function writeYourGenres() {
+  for (let i = 1; i <= 3; i++) {
+    personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+  }
+}
+
+writeYourGenres();
